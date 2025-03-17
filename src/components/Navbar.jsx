5620191fc5d34a24b1b00dfd74f165da7417977b
Jason Rocha <jasonrr19@gmail.com>
2025-03-17 00:32:39 -0700

    fixed name again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar({ setCafeDatas }) {
     <div className="d-flex justify-content-between align-items-center w-100 py-2 px-4 bg-success-subtle">
       <div className="d-flex align-items-center">
         <img height="56" className="me-3" src={logo} alt="keyboard and matcha logo" />
-        <h2>Parker's Cafe Scramble</h2>
+        <h2>Parkers Cafe Scramble</h2>
       </div>
       <div className="d-flex align-items-center gap-5">
         <SearchBar setCafeDatas={setCafeDatas} />
