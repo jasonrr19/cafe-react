24cd4022695210f0395b25cf80460e1ac7753731
Jason Rocha <jasonrr19@gmail.com>
2025-03-17 00:19:50 -0700

    website functional with api,search, and react

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,79 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import CafeList from './components/CafeList/CafeList';
 import Sidebar from './components/Sidebar/Sidebar';
+import { useState } from 'react';
 
 const App = () => {
-  const cafeDatas = [
-    {
-        "picture": "https://laptopfriendly.co/images/places/tokyo/ddsk-saigon-kitchen/ddsk-saigon-kitchen--tokyo.jpg",
-        "title": "DDSK SAIGON KITCHEN",
-        "address": "ss, 135-0061, Tokyo",
-        "criteria": []
-    },
-    {
-        "picture": "https://laptopfriendly.co/images/places/tokyo/common/common--tokyo.jpg",
-        "title": "COMMON",
-        "address": "106-0032, Tokyo",
-        "criteria": [
-            "Stable Wi-Fi",
-            "Power sockets",
-            "Quiet",
-            "Coffee",
-            "Food"
-        ]
-    },
-    {
-        "picture": "https://laptopfriendly.co/images/places/tokyo/brooklyn-parlor-shinjuku/brooklyn-parlor-shinjuku%20shinjuku-city%20brooklyn-parlor-shinjuku-shinjuku-city%20.jpg",
-        "title": "BROOKLYN PARLOR SHINJUKU",
-        "address": "Shinjuku Marui Annex B1F, 3-1-26 Shinjuku, Shinjuku-ku, Tokyo, 160-0022, Tokyo",
-        "criteria": [
-            "Stable Wi-Fi",
-            "Power sockets",
-            "Quiet",
-            "Coffee",
-            "Food"
-        ]
-    }
-];
+//   let cafeDatas = [
+//     {
+//         "picture": "https://laptopfriendly.co/images/places/tokyo/ddsk-saigon-kitchen/ddsk-saigon-kitchen--tokyo.jpg",
+//         "title": "DDSK SAIGON KITCHEN",
+//         "address": "ss, 135-0061, Tokyo",
+//         "criteria": []
+//     },
+//     {
+//         "picture": "https://laptopfriendly.co/images/places/tokyo/common/common--tokyo.jpg",
+//         "title": "COMMON",
+//         "address": "106-0032, Tokyo",
+//         "criteria": [
+//             "Stable Wi-Fi",
+//             "Power sockets",
+//             "Quiet",
+//             "Coffee",
+//             "Food"
+//         ]
+//     },
+//     {
+//         "picture": "https://laptopfriendly.co/images/places/tokyo/brooklyn-parlor-shinjuku/brooklyn-parlor-shinjuku%20shinjuku-city%20brooklyn-parlor-shinjuku-shinjuku-city%20.jpg",
+//         "title": "BROOKLYN PARLOR SHINJUKU",
+//         "address": "Shinjuku Marui Annex B1F, 3-1-26 Shinjuku, Shinjuku-ku, Tokyo, 160-0022, Tokyo",
+//         "criteria": [
+//             "Stable Wi-Fi",
+//             "Power sockets",
+//             "Quiet",
+//             "Coffee",
+//             "Food"
+//         ]
+//     }
+// ];
+const [cafeDatas, setCafeDatas] = useState([
+  {
+      "picture": "https://laptopfriendly.co/images/places/tokyo/ddsk-saigon-kitchen/ddsk-saigon-kitchen--tokyo.jpg",
+      "title": "DDSK SAIGON KITCHEN",
+      "address": "ss, 135-0061, Tokyo",
+      "criteria": []
+  },
+  {
+      "picture": "https://laptopfriendly.co/images/places/tokyo/common/common--tokyo.jpg",
+      "title": "COMMON",
+      "address": "106-0032, Tokyo",
+      "criteria": [
+          "Stable Wi-Fi",
+          "Power sockets",
+          "Quiet",
+          "Coffee",
+          "Food"
+      ]
+  },
+  {
+      "picture": "https://laptopfriendly.co/images/places/tokyo/brooklyn-parlor-shinjuku/brooklyn-parlor-shinjuku%20shinjuku-city%20brooklyn-parlor-shinjuku-shinjuku-city%20.jpg",
+      "title": "BROOKLYN PARLOR SHINJUKU",
+      "address": "Shinjuku Marui Annex B1F, 3-1-26 Shinjuku, Shinjuku-ku, Tokyo, 160-0022, Tokyo",
+      "criteria": [
+          "Stable Wi-Fi",
+          "Power sockets",
+          "Quiet",
+          "Coffee",
+          "Food"
+      ]
+  }
+]);
 
-const getCafes = () => {
 
-};
   return (
   <div className="app-frame">
-    <Navbar />
+    <Navbar setCafeDatas={setCafeDatas} />
+    {/* <button onClick={getCafes} >Get All Cafes!!</button> */}
     <div className="app-body">
       <Sidebar />
       <CafeList cafeDatas={cafeDatas}/>
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,12 +3,25 @@
 import React from 'react';
 import './Searchbar.css';
 
-function Searchbar() {
+function Searchbar({ setCafeDatas }) {
+
+  const getCafes = (event) => {
+    console.log("Getting cafes...");
+    const keyword = event.currentTarget.value;
+    const url = `https://rails-cafe-api-9a78e13d2364.herokuapp.com/api/v1/cafes?title=${keyword}`;
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        // cafeDatas = data;
+        setCafeDatas(data);
+      });
+  };
 
   return (
     <div className="searchbar input-group">
       <span className="input-group-text"><i className="fa-solid fa-magnifying-glass"></i></span>
-      <input placeholder="Starbucks" type="text" className="form-control" />
+      <input onChange={getCafes} placeholder="Starbucks" type="text" className="form-control" />
     </div>
   );
 }
